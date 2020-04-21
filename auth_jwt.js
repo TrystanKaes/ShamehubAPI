2d18b1147dfd10268fe2c17a507850761f6ff247
e-slick <33139655+e-slick@users.noreply.github.com>
2020-04-20 22:46:00 -0600

    Merging with master

--- auth_jwt.js
+++ auth_jwt.js
@@ -4,11 +4,12 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var User = require('./Users');
 
+
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
 opts.secretOrKey = process.env.SECRET_KEY;
-
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+//opts.secretOrKey = "ADL;ASALK;DAKLJKL";
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findById(jwt_payload.id, function (err, user) {
         if (user) {
             done(null, user);

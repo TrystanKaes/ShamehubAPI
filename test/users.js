69dd4f78c88766ad5ce4dcb145f30c36a2e11570
Trystan Kaes <60720259+TrystanKaes@users.noreply.github.com>
2020-04-23 13:17:45 -0600

    Update users.js

--- test/users.js
+++ test/users.js
@@ -1,11 +1,12 @@
 //Require the dev-dependencies
 let envPath = __dirname + "/../.env"
-require('dotenv').config({path:envPath})
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 var User = require('../Users');
 let should = chai.should();
+//Add this back in for local developement
+// require('dotenv').config({path:envPath})
 
 chai.use(chaiHttp);
 
